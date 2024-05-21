08e6bae4495659c29c47d68677292ea33d458eb5
Lap Wang Wong <leonwonglww@gmail.com>
2024-05-21 12:03:30 -0400

    Added cart and cartItem entity

---
+++ src/main/java/dev/leonwong/model/CartItem.java
@@ -0,0 +1,23 @@
+package dev.leonwong.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @ManyToOne
+    private Food food;
+}
