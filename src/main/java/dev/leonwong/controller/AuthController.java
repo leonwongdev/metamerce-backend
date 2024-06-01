188e9c1ca0a03e397b8b0d672327dcd560f7e435
Lap Wang Wong <leonwonglww@gmail.com>
2024-06-01 19:33:24 -0400

    Added signin method

--- src/main/java/dev/leonwong/controller/AuthController.java
+++ src/main/java/dev/leonwong/controller/AuthController.java
@@ -1,17 +1,31 @@
 package dev.leonwong.controller;
 
 import dev.leonwong.config.JwtProvider;
+import dev.leonwong.model.Cart;
 import dev.leonwong.model.User;
+import dev.leonwong.model.UserRole;
 import dev.leonwong.repository.CartRepository;
 import dev.leonwong.repository.UserRepository;
+import dev.leonwong.request.LoginRequest;
 import dev.leonwong.response.AuthResponse;
 import dev.leonwong.service.CustomerUserDetailsService;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -33,6 +47,7 @@ public class AuthController {
     @Autowired
     private CartRepository cartRepository;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
 
         User dbUser = userRepository.findByEmail(user.getEmail());
@@ -48,6 +63,57 @@ public class AuthController {
         newUser.setPassword(passwordEncoder.encode(user.getPassword()));
 
         User savedUser = userRepository.save(newUser);
-        return null;
+
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register Success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest loginRequest) throws BadRequestException {
+
+        String username = loginRequest.getEmail();
+
+        String password = loginRequest.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register Success");
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+        authResponse.setRole(UserRole.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) throws BadRequestException {
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if (userDetails == null) {
+            throw new BadRequestException("Invalid username, username not found.");
+        }
+
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+    }
+
+
 }
--- src/main/java/dev/leonwong/response/AuthResponse.java
+++ src/main/java/dev/leonwong/response/AuthResponse.java
@@ -1,7 +1,9 @@
 package dev.leonwong.response;
 
 import dev.leonwong.model.UserRole;
+import lombok.Data;
 
+@Data
 public class AuthResponse {
 
     private String jwt;
